8068d7f9f220020e167bb331f0ed17d9e5d29929
slashh1 <ejazhussain007@gmail.com>
2021-09-26 03:56:37 +0530

    nested components

--- src/index.js
+++ src/index.js
@@ -2,17 +2,39 @@ import React from "react";
 import reactDom from "react-dom";
 
 //nested components
-
-function Greeting() {
+function BookList() {
   return (
-    <div>
-      <Person />
-      <Message />
-    </div>
+    <section>
+      <Book />
+    </section>
   );
 }
-const Person = () => <h2>Jhon Doe</h2>;
-const Message = () => {
-  return <p>This is a message</p>;
+
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
 };
-reactDom.render(<Greeting />, document.getElementById("root"));
+
+const Image = () => {
+  return (
+    <img
+      src="https://cdn.pixabay.com/photo/2017/03/15/16/28/alchemy-2146679_640.jpg"
+      alt="The alcmeist cover"
+      width="250px"
+      height="250px"
+    />
+  );
+};
+const Title = () => {
+  return <h1>The Alchemist</h1>;
+};
+const Author = () => {
+  return <p>Paulo Coelho</p>;
+};
+
+reactDom.render(<BookList />, document.getElementById("root"));
