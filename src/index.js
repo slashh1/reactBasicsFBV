5d56ecdb2a2087d0fcf3e096c1819614a7553854
slashh1 <ejazhussain007@gmail.com>
2021-09-26 23:57:11 +0530

    react synthetic events inline call, function call and arrow function call

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import reactDom from "react-dom";
 import "./index.css";
-//DRY principle for props using spread operator and removing key warning
+//React events
+//synthetic events : https://reactjs.org/docs/events.html
 
 const books = [
   {
@@ -34,14 +35,34 @@ function BookList() {
   );
 }
 
+//onClick , onMouseOver
+
 const Book = (props) => {
-  console.log(props);
+  // console.log(props);
   const { img, title, author } = props;
+  const clickHandler = (e) => {
+    console.log(e, e.target);
+    alert(title);
+  };
+  const clickAuthor = (author) => {
+    console.log(author);
+  };
   return (
-    <article className="book">
+    <article className="book" onMouseOver={() => console.log(title)}>
+      {/* above example shows we can use inline functions on synthetic events */}
       <img src={img} alt="" width="250px" height="150px" />
       <h1>{title}</h1>
       <h4>{author}</h4>
+      <button onClick={clickHandler}>Title</button>
+      <button
+        onClick={() => {
+          clickAuthor(author);
+        }}
+        // if i pass the function directly as  clickAuthor(author) without using an arrow function
+        // it will call the function without click on load hence to make it load only on cliek we wrap it with an arrow function
+      >
+        Autor
+      </button>
     </article>
   );
 };
